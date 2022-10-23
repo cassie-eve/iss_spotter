f32c976c47d903b22f0f02ce128944a2a00d1321
Cassandra Keddis <cassandra@thinkific.com>
2022-10-23 14:38:22 -0700

    add final function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchCoordsByIP, fetchMyIP, fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -15,9 +15,26 @@ const { fetchCoordsByIP, fetchMyIP, fetchISSFlyOverTimes } = require('./iss');
 //   console.log(data);
 // })
 
-fetchISSFlyOverTimes({ latitude: 49.2827291, longitude: -123.1207375 }, (error, data) => {
-  if (error) {
-    console.log('Oh no!', error);
+// fetchISSFlyOverTimes({ latitude: 49.2827291, longitude: -123.1207375 }, (error, data) => {
+//   if (error) {
+//     console.log('Oh no!', error);
+//   }
+//   console.log(data);
+// });
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
-  console.log(data);
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
